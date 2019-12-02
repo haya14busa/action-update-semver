6d3bdd8aee56c190910a85b7d6be16bb868f9f10
haya14busa <haya14busa@gmail.com>
2019-12-02 09:12:30 +0000

    test push from workflow file

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,9 @@ fi
 # Set up Git.
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git remote set-url origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+# git remote set-url origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
 # Update MAJOR/MINOR tag
 git tag -fa ${MAJOR} -m "Release ${TAG}"
 git tag -fa ${MINOR} -m "Release ${TAG}"
-git push --tags --force
+# git push --tags --force
