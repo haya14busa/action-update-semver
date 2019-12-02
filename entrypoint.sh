9c5e236994bb200c2f4c2e9ef0466129fa7fc967
haya14busa <haya14busa@gmail.com>
2019-12-02 09:42:28 +0000

    Clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,9 @@ MINOR="${TAG%.*}"              # v1.2
 MAJOR="${MINOR%.*}"            # v1
 
 if [ "${GITHUB_REF}" = "${TAG}" ]; then
-  echo "This workflow is not triggered by tag: GITHUB_REF=${GITHUB_REF}"
+  echo "This workflow is not triggered by tag push: GITHUB_REF=${GITHUB_REF}"
   exit 1
 fi
-
-echo ::set-output name=minor_tag::${MINOR}
-echo ::set-output name=major_tag::${MAJOR}
 
 # Set up Git.
 git config user.name "${GITHUB_ACTOR}"
