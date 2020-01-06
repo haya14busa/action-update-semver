fa3fc1838c8097544a84773e8b8c609c901047f2
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-01-06 08:42:06 +0100

    Add MAJOR_VERSION_TAG_ONLY variable to tag only the major versions #2
    * Add MAJOR_VERSION_TAG_ONLY variable to tag only the major versions #2
    * `entrypoint.sh` modified to pass shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -x
 
-cd "${GITHUB_WORKSPACE}"
+cd "${GITHUB_WORKSPACE}" || exit
 
 # Set up variables.
 TAG="${INPUT_TAG:-${GITHUB_REF#refs/tags/}}" # v1.2.3
 MINOR="${TAG%.*}"                            # v1.2
 MAJOR="${MINOR%.*}"                          # v1
+MAJOR_VERSION_TAG_ONLY=${INPUT_MAJOR_VERSION_TAG_ONLY:-}
 
 if [ "${GITHUB_REF}" = "${TAG}" ]; then
   echo "This workflow is not triggered by tag push: GITHUB_REF=${GITHUB_REF}"
@@ -20,10 +21,10 @@ git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 # Update MAJOR/MINOR tag
-git tag -fa ${MAJOR} -m "${MESSAGE}"
-git tag -fa ${MINOR} -m "${MESSAGE}"
+git tag -fa "${MAJOR}" -m "${MESSAGE}"
+[ "${MAJOR_VERSION_TAG_ONLY}" = "true" ] || [ "${MAJOR_VERSION_TAG_ONLY}" = "yes" ] && git tag -fa "${MINOR}" -m "${MESSAGE}"
 
 # Push
-git remote set-url origin https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
-git push --force origin ${MINOR}
-git push --force origin ${MAJOR}
+git remote set-url origin "https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+[ "${MAJOR_VERSION_TAG_ONLY}" = "true" ] || [ "${MAJOR_VERSION_TAG_ONLY}" = "yes" ] git push --force origin "${MINOR}"
+git push --force origin "${MAJOR}"
